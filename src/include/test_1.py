a014b74d623207be9369a333ea3eac56e6a15411
Patrick Kosierb <kosierbp@mcmaster.ca>
2021-09-20 16:53:36 -0400

    init

---
+++ src/include/test_1.py
@@ -0,0 +1,11 @@
+##  
+#  @file test_1.py
+#  
+#  @brief Test import file
+# 
+#  @note The imported files do-not need to be in a different directory, not sure how python dev is strutured
+##  
+
+## This is a test function it adds 1 to x ##
+def function1(x):
+    print(x+1)
