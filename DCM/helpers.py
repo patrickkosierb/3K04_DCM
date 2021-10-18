b5e3b6fcfd487d36187a2ca8ce2bcc17b8b83ee2
Patrick Kosierb <kosierbp@mcmaster.ca>
2021-10-18 17:04:10 -0400

    added comments to helper.py

--- DCM/helpers.py
+++ DCM/helpers.py
@@ -1,3 +1,6 @@
+# @ file: helpers.py
+# @ brief: helpers file contains functionality common accross all modules
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QStackedWidget	
 import config
@@ -6,6 +9,7 @@ def go_to_page(page):
 	config.widget.addWidget(page)
 	config.widget.setCurrentIndex(config.widget.currentIndex()+1)
 
+# checks if user input is a number, then sets current pacemaker parameter text box to the input
 def check_and_update(user, current, label):
 	if(user.isdigit() and int(user)<255):
 		current.setText(user)
@@ -15,6 +19,7 @@ def check_and_update(user, current, label):
 	else:
 		label.setText("Invalid*")
 
+# updates lists of values
 def update_current(user, current, label):
 	for i in range(0,len(user)):
 		check_and_update(user[i], current[i], label[i])
