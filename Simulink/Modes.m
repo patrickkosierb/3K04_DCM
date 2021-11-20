4712aba42d033605b7e27bba6d79c031b595cd25
Wehbinum <omarw.wehbe@gmail.com>
2021-11-20 14:06:37 -0500

    Added the R modes to Modes.m/Adjusted the model for hardware hiding/Setup 70% of the Rate Adaptive Subsystem

--- Simulink/Modes.m
+++ Simulink/Modes.m
@@ -1,11 +1,18 @@
 classdef Modes < Simulink.IntEnumType
   enumeration
+    % The rate adaptive modes come after the non-rate adaptive and must
+    % start with AOOR
     NO_PACE(0)
     AOO(1)
     VOO(2)
     AAI(3)
     VVI(4)
     DOO(5)
+    AOOR(6)
+    VOOR(7)
+    AAIR(8)
+    VVIR(9)
+    DOOR(10)
   end
 end
 
